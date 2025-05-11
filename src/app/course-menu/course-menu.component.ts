953b8a8d6fbc4a9ae5292226f1f5b8c369050903
ArtemPavshak <124794623+ArtemPavshak@users.noreply.github.com>
2025-05-11 16:56:09 +0300

    add category-course and menu-course

---
+++ src/app/course-menu/course-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+@Component({
+  selector: 'app-course-menu',
+  imports: [SidebarComponent],
+  templateUrl: './course-menu.component.html',
+  styleUrl: './course-menu.component.css'
+})
+export class CourseMenuComponent {
+
+}
