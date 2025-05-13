78383783743c24b0bd4819aa1cb108c8d84119fe
ArtemPavshak <124794623+ArtemPavshak@users.noreply.github.com>
2025-05-13 20:23:58 +0300

    create module-menu

--- src/app/module-menu/module-menu.component.ts
+++ src/app/module-menu/module-menu.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-module-menu',
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './module-menu.component.html',
   styleUrl: './module-menu.component.css'
 })
